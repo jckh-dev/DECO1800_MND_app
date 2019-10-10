e2e98d5b7c4aa4e21d37da03cc9a1041fb07e9ec
JCKH <55143530+jckh-dev@users.noreply.github.com>
2019-10-10 21:20:13 +1000

    fully formed tour template page now ready for insertion of proper content and navigation JS code

--- guidedtour.php
+++ guidedtour.php
@@ -9,51 +9,149 @@ include('includes/header.php');
 ?>
 
 <aside class="box txtbox">
-    <h1>WELCOME TO THE GUIDED TOUR</h1><br>
+    <h1>Maybe a dynamic PHP call to change the title depending on which tour article ID is active?</h1><br>
 </aside>
 
 </section>
 
 <section class="gridwrap2">
 
-    <article class="box">
-        <p>Each area of the exhibit +</p><br>
+    <article class="infobox" id="tourwelcome">
+            
+        <h1>WELCOME WELCOME WELCOME</h1>
+            <p>"THIS IS A PLACEHOLDER FOR WEEEEEELCOMING Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
         
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <aside class="box welcomebtns">
+            <aside class="demo">
+                <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Home</h1></a>
+            </aside>
+
+            <aside class="continue">
+                <a href=""><h1>Continue <i class="fas fa-chevron-circle-right"></i></h1></a>
+            </aside>
+        </aside>
+
+    </article>
+    
+    <article class="infobox" id="bushfires">
+        
+        <img class="tourgraph" src="images\examplegraph.png" alt="Major Environmental Events Over Time">    
+    
+        <h1>BUSHFIRES</h1>
+            <p>"THIS IS A PLACEHOLDER FOR BUSHFIIIARS Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
+        
+        <aside class="box welcomebtns">
+            <aside class="demo">
+                <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Welcome</h1></a>
+            </aside>
+
+            <aside class="continue">
+                <a href=""><h1>Floods <i class="fas fa-chevron-circle-right"></i></h1></a>
+            </aside>
+        </aside>
+
     </article>
 
-    <article class="box" id="demoinfo">
-        <h1>DEMO HELP</h1>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    <article class="infobox" id="floods">
+        
+        <img class="tourgraph" src="images\examplegraph.png" alt="Major Environmental Events Over Time" width="500" height="600">    
+
+        <h1>FLOODS</h1>
+        <p>"THIS IS A PLACEHOLDER FOR FLOOOODS Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
         aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
         
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <aside class="box welcomebtns">
+            <aside class="demo">
+               <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Bushfires</h1></a>
+            </aside>
 
-        <button id="democlose" class="idbutton">Close Demo Box</button>
+            <aside class="continue">
+                <a href="journey.php"><h1>Cyclones <i class="fas fa-chevron-circle-right"></i></h1></a>
+            </aside>
+        </aside>
+
+    </article>
+
+    <article class="infobox" id="cyclones">
+        
+        <img class="tourgraph" src="images\examplegraph.png" alt="Major Environmental Events Over Time" width="500" height="600">    
+            
+        <h1>CYCLONES</h1>
+        <p>"THIS IS A PLACEHOLDER FOR CYCLOOOONES Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
+        
+        <aside class="box welcomebtns">
+            <aside class="demo">
+                <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Floods</h1></a>
+            </aside>
+
+            <aside class="continue">
+                <a href="journey.php"><h1>Storms <i class="fas fa-chevron-circle-right"></i></h1>
+            </a>
+            </aside>
+        </aside>
+    </article>
+
+    <article class="infobox" id="storms">
+        
+        <img class="tourgraph" src="images\examplegraph.png" alt="Major Environmental Events Over Time" width="500" height="600">    
+    
+        <h1>SEVERE STORMS</h1>
+        <p>"THIS IS A PLACEHOLDER FOR STOOOORMS Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
+        
+        <aside class="box welcomebtns">
+            <aside class="demo">
+                <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Cyclones</h1></a>
+            </aside>
+
+            <aside class="continue">
+                <a href="journey.php"><h1>Major Events <i class="fas fa-chevron-circle-right"></i></h1>
+            </a>
+            </aside>
+        </aside>
+    </article>
+
+    <article class="infobox" id="majorevents">
+        <h1>MAJOR ENVIRONMENTAL EVENTS</h1>
+
+        <img class="tourgraph" src="images\examplegraph.png" alt="Major Environmental Events Over Time" width="500" height="600">
+
+        <p>"THIS IS A PLACEHOLDER FOR MAJOR EVEEEENTS Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
+        
+        
+        <!-- TURN THIS BUTTON INTO A FORM DIRECTING TO GAME.PHP INITIATED WITH TOUR GAME -->
+        <aside class="box welcomebtns">
+            <aside class="demo">
+                <a href=""><h1><i class="fas fa-chevron-circle-left"></i> Storms</h1></a>
+            </aside>
+
+            <aside class="continue">
+                <a href="journey.php"><h1>Play Game <i class="fas fa-chevron-circle-right"></i></h1>
+            </a>
+            </aside>
+        </aside>
     </article>
 
 </section>
 
-<section class="gridwraper3">
-
-<aside class="box welcomebtns">
-<aside class="demo">
-        <h1>Demo <i class="fas fa-mobile-alt"></i></h1>
-</aside>
-
-<aside class="continue">
-    <a href="journey.php"><h1>Continue <i class="fas fa-chevron-circle-right"></i></h1>
-    </a>
-</aside>
-</aside>
-</section>
-</div>
-
 <script src="js/app.js"></script>
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/jquery-ui.min.js"></script>
-</body>
+
+<?php
+include('includes/footer.php');
+?>
