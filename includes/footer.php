794e5ee25da9b52baa4110722a05252e94980b3c
JCKH <55143530+jckh-dev@users.noreply.github.com>
2019-10-10 16:39:23 +1000

    footer adjustments

--- includes/footer.php
+++ includes/footer.php
@@ -2,19 +2,18 @@
 
 <section class="gridwrap3">
 
-<footer class="pagination">
-  <div class="pagination-container">
-      <div class="pagination-hover-overlay"></div>
-          <!--
-          <a href="index.php" class="pagination-page-number">Welcome</a>
-          <a href="journey.php" class="pagination-page-number">Home</a>
-          <a href="game.php" class="pagination-page-number">Game</a>
-          <a href="map.php" class="pagination-page-number">Map</a>
-          <a href="scoreboard.php" class="pagination-page-number">Scores</a>
-          <a href="ending.php" class="pagination-page-number">Finish</a> 
+<footer class="breadcrumb">
+          
+          <!-- <a href="index.php" class="pagination-page-number" id="fwelcome">Welcome</a>
+          <a href="journey.php" class="pagination-page-number" id="fhome">Home</a>
+          <a href="guidedtour.php" class="pagination-page-number" id="ftour">Tour</a>
+          <a href="game.php" class="pagination-page-number" id="fgame">Game</a>
+          <a href="map.php" class="pagination-page-number" id="fmap">Map</a>
+          <a href="scoreboard.php" class="pagination-page-number id="fscores">Scores</a>
+          <a href="ending.php" class="pagination-page-number" id="fending">Finish</a> 
           -->
-  </div>
 </footer>
+
 </section>
 </div>
 <script src="js/jquery-3.4.1.min.js"></script>
--- js/function.js
+++ js/function.js
@@ -1,80 +1,42 @@
-let paginationLeftPos = "20px";
-let paginationOpacity = 0;
-let checkPaginationClick = 0;
- 
-$(".pagination-page-number").click(function () {
-  $(".pagination-page-number").removeClass("active");
-  $(this).addClass("active");
-  paginationLeftPos = $(this).prop("offsetLeft") + "px";
-  paginationOpacity = 1;
-  checkPaginationClick = 1;
- 
-  $(".pagination-hover-overlay").css({
-    left: paginationLeftPos,
-    backgroundColor: "#e60023",
-    opacity: paginationOpacity });
- 
-  $(this).css({
-    color: "#fff" });
- 
-});
- 
-$(".pagination-page-number").hover(
-function () {
-  paginationOpacity = 1;
-  $(".pagination-hover-overlay").css({
-    backgroundColor: "#fa4949",
-    left: $(this).prop("offsetLeft") + "px",
-    opacity: paginationOpacity });
- 
- 
-  $(".pagination-page-number.active").css({
-    color: "#212121" });
- 
- 
-  $(this).css({
-    color: "#fff" });
- 
-},
-function () {
-  if (checkPaginationClick) {
-    paginationOpacity = 1;
-  } else {
-    paginationOpacity = 0;
-  }
- 
-  $(".pagination-hover-overlay").css({
-    backgroundColor: "#e60023",
-    opacity: paginationOpacity,
-    left: paginationLeftPos });
- 
- 
-  $(this).css({
-    color: "#212121" });
- 
- 
-  $(".pagination-page-number.active").css({
-    color: "#fff" });
- 
-});
-
 // breadcrumb insert/delete james
 var pathname = window.location.pathname.split("/");
 var last = pathname[pathname.length - 1];
+
 if (last == "journey.php") {
-  $(".pagination-container").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".pagination-container").append('<a href="" class="pagination-page-number">Home</a>');
+  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
+  $(".breadcrumb").append('<a href="" class="pagination-page-number">Home</a>');
+  $("#fhome").addClass("footer-active")
 } else if (last == "game.php") {
-  $(".pagination-container").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".pagination-container").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".pagination-container").append('<a href="" class="pagination-page-number">Game</a>');
+  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
+  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
+  $(".breadcrumb").append('<a href="" class="pagination-page-number">Game</a>');
+  $("#fgame").addClass("footer-active")
 } else if (last == "scoreboard.php") {
-  $(".pagination-container").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".pagination-container").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".pagination-container").append('<a href="" class="pagination-page-number">Leaderboard</a>');
+  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
+  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
+  $(".breadcrumb").append('<a href="" class="pagination-page-number">Leaderboard</a>');
+  $("#fscores").addClass("footer-active")
 } else if (last == "ending.php") {
-  $(".pagination-container").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".pagination-container").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".pagination-container").append('<a href="" class="pagination-page-number">Game</a>');
-  $(".pagination-container").append('<a href="" class="pagination-page-number">Finish</a>');
+  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
+  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
+  $(".breadcrumb").append('<a href="" class="pagination-page-number">Game</a>');
+  $(".breadcrumb").append('<a href="" class="pagination-page-number">Finish</a>');
+  $("#fending").addClass("footer-active")
+}
+
+
+if (last == "journey.php"){
+ $("#fhome")
+}
+else if (last == "game.php"){
+
+}
+else if (last == "scoreboard.php"){
+
+}
+else if (last == "ending.php"){
+
+}
+else if (last == "map.php"){
+
 }
