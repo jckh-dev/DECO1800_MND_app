6e4a3160d44171dec996cbdf6d6423d89766a015
JCKH <55143530+jckh-dev@users.noreply.github.com>
2019-10-10 21:24:58 +1000

    >minor change to breadcrumbs for guided tour

--- js/function.js
+++ js/function.js
@@ -2,7 +2,7 @@
 
 var pathname = window.location.pathname.split("/");
 var last = pathname[pathname.length - 1];
-var welcome = '<a href="index.php" class="breadcrumb-item" id="fwelcome">Welcome</a>';
+var welcome = '<a href="welcomemsg.php" class="breadcrumb-item" id="fwelcome">Welcome</a>';
 var home = '<a href="journey.php" class="breadcrumb-item" id="fhome">Home</a>';
 var tour = '<a href="guidedtour.php" class="breadcrumb-item" id="ftour">Tour</a>';
 var game = '<a href="game.php" class="breadcrumb-item" id="fgame">Game</a>';
@@ -14,22 +14,32 @@ if (last == "journey.php") {
   $(".breadcrumb").append(welcome);
   $(".breadcrumb").append(home);
   $("#fhome").addClass("footer-active")
-} else if (last == "game.php") {
+} 
+else if (last == "guidedtour.php") {
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
+  $(".breadcrumb").append(tour);
+  $("#ftour").addClass("footer-active")
+}
+else if (last == "game.php") {
   $(".breadcrumb").append(welcome);
   $(".breadcrumb").append(home);
   $(".breadcrumb").append(game);
   $("#fgame").addClass("footer-active")
-} else if (last == "map.php") {
+} 
+else if (last == "map.php") {
   $(".breadcrumb").append(welcome);
   $(".breadcrumb").append(home);
   $(".breadcrumb").append(map);
   $("#fmap").addClass("footer-active")
-} else if (last == "scoreboard.php") {
+} 
+else if (last == "scoreboard.php") {
   $(".breadcrumb").append(welcome);
   $(".breadcrumb").append(home);
   $(".breadcrumb").append(scores);
   $("#fscores").addClass("footer-active")
-} else if (last == "ending.php") {
+} 
+else if (last == "ending.php") {
   $(".breadcrumb").append(welcome);
   $(".breadcrumb").append(home);
   $(".breadcrumb").append(game);
