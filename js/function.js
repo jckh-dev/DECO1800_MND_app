eea4829ed8adb9bb2ce12152626b3254a5e4dd80
JCKH <55143530+jckh-dev@users.noreply.github.com>
2019-10-10 17:00:25 +1000

    persistent highlight on the footer

--- js/function.js
+++ js/function.js
@@ -1,42 +1,54 @@
 // breadcrumb insert/delete james
 var pathname = window.location.pathname.split("/");
 var last = pathname[pathname.length - 1];
+var welcome = '<a href="index.php" class="pagination-page-number" id="fwelcome">Welcome</a>';
+var home = '<a href="journey.php" class="pagination-page-number" id="fhome">Home</a>';
+var tour = '<a href="guidedtour.php" class="pagination-page-number" id="ftour">Tour</a>';
+var game = '<a href="game.php" class="pagination-page-number" id="fgame">Game</a>';
+var map = '<a href="map.php" class="pagination-page-number" id="fmap">Map</a>';
+var scores = '<a href="scoreboard.php" class="pagination-page-number id="fscores">Scores</a>';
+var finish = '<a href="ending.php" class="pagination-page-number" id="fending">Finish</a> ';
 
 if (last == "journey.php") {
-  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".breadcrumb").append('<a href="" class="pagination-page-number">Home</a>');
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
   $("#fhome").addClass("footer-active")
 } else if (last == "game.php") {
-  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".breadcrumb").append('<a href="" class="pagination-page-number">Game</a>');
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
+  $(".breadcrumb").append(game);
   $("#fgame").addClass("footer-active")
+} else if (last == "map.php") {
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
+  $(".breadcrumb").append(map);
+  $("#fmap").addClass("footer-active")
 } else if (last == "scoreboard.php") {
-  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".breadcrumb").append('<a href="" class="pagination-page-number">Leaderboard</a>');
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
+  $(".breadcrumb").append(scores);
   $("#fscores").addClass("footer-active")
 } else if (last == "ending.php") {
-  $(".breadcrumb").append('<a href="index.php" class="pagination-page-number">Welcome</a>');
-  $(".breadcrumb").append('<a href="journey.php" class="pagination-page-number">Home</a>');
-  $(".breadcrumb").append('<a href="" class="pagination-page-number">Game</a>');
-  $(".breadcrumb").append('<a href="" class="pagination-page-number">Finish</a>');
+  $(".breadcrumb").append(welcome);
+  $(".breadcrumb").append(home);
+  $(".breadcrumb").append(game);
+  $(".breadcrumb").append(finish);
   $("#fending").addClass("footer-active")
 }
 
 
-if (last == "journey.php"){
- $("#fhome")
-}
-else if (last == "game.php"){
+// if (last == "journey.php"){
+//  $("#fhome")
+// }
+// else if (last == "game.php"){
 
-}
-else if (last == "scoreboard.php"){
+// }
+// else if (last == "scoreboard.php"){
 
-}
-else if (last == "ending.php"){
+// }
+// else if (last == "ending.php"){
 
-}
-else if (last == "map.php"){
+// }
+// else if (last == "map.php"){
 
-}
+// }
